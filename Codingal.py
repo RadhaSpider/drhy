93c7fec9cc56af7be6a3b70b8d1e7fb0f706c22a
RadhaSpider <sanjusingh70243@gmail.com>
2025-03-09 19:58:15 +0530

    Add files via upload

---
+++ Codingal.py
@@ -0,0 +1,11 @@
+#Enter the value of terms
+n = int(input("Enter the value of terms : "))
+sum = 0 #initialize
+i = 1 #initialize
+while i<=n: #loop will run from 1 to i 
+    sum=sum+i
+    i = i+1
+print("\nsum =", sum)
+
+
+
